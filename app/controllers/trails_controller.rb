2d2ea5e6fb006dcb798945ad82bbc45ba84fba57
Katelyn <katelyn.rimkunas@gmail.com>
2018-11-04 14:52:05 -0700

    summary of user stories

---
+++ app/controllers/trails_controller.rb
@@ -0,0 +1,22 @@
+
+
+class TrailController < ApplicationController
+
+
+
+
+
+  def show
+    # name
+    # address
+    # -----
+    # total number of trips that include this hike
+    # name of every trip, & total length of trip
+  end
+
+
+
+
+
+
+end
---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,27 @@
+
+class TripsController < ApplicationController
+
+
+  def index
+    # list of all trips
+    # title are links to show
+  end
+
+
+  def show
+    # trials included in that trip
+    # name, address, length of each trial
+    # trail names link to trail show
+    # --- stats ---
+    # total distance of trip (all trials)
+    # average distance of each trail on that trip
+    # longest trail of that trip (longest distance, not just the name)
+    # shortest trail of that trip (shortest distance, not just the name)
+  end
+
+
+
+
+
+
+end
